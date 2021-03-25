a64490471e0b98ae777d17b413da68c8fec50eb0
cjkkkk <shanbinke@gmail.com>
2021-03-25 15:44:10 +0800

    add simple interval analysis

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 ADD = 0
 MUL = 1
 DIV = 2
-mapping = ["+", "*", "/"]
+SUB = 3
+mapping = ["+", "*", "/", "-"]
 
 class Expr:
     def __init__(self, *subexprs):
@@ -10,19 +11,33 @@ class Expr:
     def __add__(self, other):
         if isinstance(other, int):
             other = ConstExpr(other)
+        if isinstance(self, ConstExpr) and isinstance(other, ConstExpr):
+            return ConstExpr(self.val + other.val)
         return BinaryExpr(self, other, ADD)
 
     def __mul__(self, other):
         if isinstance(other, int):
             other = ConstExpr(other)
+        if isinstance(self, ConstExpr) and isinstance(other, ConstExpr):
+            return ConstExpr(self.val * other.val)
         return BinaryExpr(self, other, MUL)
 
     def __floordiv__(self, other):
         if isinstance(other, int):
             other = ConstExpr(other)
+        if isinstance(self, ConstExpr) and isinstance(other, ConstExpr):
+            return ConstExpr(self.val // other.val)
         return BinaryExpr(self, other, DIV)
+    
+    def __sub__(self, other):
+        if isinstance(other, int):
+            other = ConstExpr(other)
+        if isinstance(self, ConstExpr) and isinstance(other, ConstExpr):
+            return ConstExpr(self.val - other.val)
+        return BinaryExpr(self, other, SUB)
 
     __radd__ = __add__
+    __rmul__ = __mul__
 
     def CUDA_codegen(self):
         raise NotImplementedError
@@ -34,6 +49,9 @@ class BinaryExpr(Expr):
         self.right = right
         self.type = type
 
+    def __str__(self):
+        return "{0} {1} {2}".format(self.left, mapping[self.type], self.right)
+
     def CUDA_codegen(self):
         return "{0} {1} {2}".format(self.left.CUDA_codegen(), mapping[self.type], self.right.CUDA_codegen())
 
@@ -41,6 +59,9 @@ class VarExpr(Expr):
     def __init__(self, name):
         super().__init__()
         self.name = name
+
+    def __str__(self):
+        return self.name
 
     def CUDA_codegen(self):
         return self.name
@@ -50,17 +71,23 @@ class ConstExpr(Expr):
         super().__init__()
         self.val = val
 
+    def __str__(self):
+        return str(self.val)
+
     def CUDA_codegen(self):
         return str(self.val)
 
 class IterVar(Expr):
-    def __init__(self, name, start, length, stride=1):
+    def __init__(self, name, start, end, stride=1):
         super().__init__()
         self.name = name
         self.start = ConstExpr(start) if isinstance(start, int) else start
-        self.length = ConstExpr(length) if isinstance(length, int) else length
+        self.end = ConstExpr(end) if isinstance(end, int) else end
         self.stride = ConstExpr(stride) if isinstance(stride, int) else stride
         self.attached_computation = []
+
+    def __str__(self):
+        return "{0}: [{1}, {2})".format(self.name, self.start, self.end)
 
     def CUDA_codegen(self):
         return self.name
@@ -77,6 +104,9 @@ class TensorSliceExpr(Expr):
     def __setitem__(self, index, item):
         raise NotImplementedError
     
+    def __str__(self):
+        return self.tensor.name + "[" + ", ".join([index for index in self.index]) + "]" 
+
     def CUDA_codegen(self):
         return self.tensor.name + "[" + ", ".join([index.CUDA_codegen() for index in self.index]) + "]" 
 
@@ -96,6 +126,9 @@ class TensorExpr(Expr):
     def __setitem__(self, index, item):
         raise NotImplementedError
     
+    def __str__(self):
+        return self.tensor.name
+
     def CUDA_codegen(self, indent=0):
         res = ""
         closing = "".join(["    " * (len(self.axis) - 1 - i + indent) + "}\n" for i in range(len(self.axis))])
@@ -104,7 +137,7 @@ class TensorExpr(Expr):
             res += "    " * (i + indent) + "for (int {0} = {1}; {0} < {2} ; {0} += {3};) {{\n".format(
                 axis.name, 
                 axis.start.CUDA_codegen(),
-                axis.length.CUDA_codegen(), # TODO: start + length
+                axis.end.CUDA_codegen(),
                 axis.stride.CUDA_codegen())
             for computation in axis.attached_computation:
                 res += computation.CUDA_codegen(i + 1)
@@ -147,7 +180,7 @@ def split(tensor, axis_idx, factor):
     for i, axis in enumerate(old_axis):
         if i == axis_idx:
             # new axis
-            outer = IterVar(axis.name + ".outer", 0, axis.length // factor if not isinstance(axis.length, ConstExpr) else ConstExpr(axis.length.val // factor))
+            outer = IterVar(axis.name + ".outer", 0, axis.end // factor)
             inner = IterVar(axis.name + ".inner", 0, ConstExpr(factor))
             new_axis.append(outer)
             new_axis.append(inner)
@@ -169,12 +202,34 @@ def reorder(tensor, new_axis_idx):
 def compute_at(producer, consumer, axis_idx):
     consumer.axis[axis_idx].attached_computation.append(producer)
 
-def infer_bound(tensor, index):
+def infer_bound(tensor, intervals):
     # TODO: only infer when there are const axis
-    pass
+    for idx, axis in enumerate(tensor.axis):
+        for producer_tensor in tensor.producer_tensor:
+            infer_bound(producer_tensor.tensor, [])
+
+def evaluate_expr_range(expr, frozen_axis):
+    # TODO: skip frozen axis
+    if isinstance(expr, IterVar):
+        return expr
+    elif isinstance(expr, BinaryExpr):
+        left = evaluate_expr_range(expr.left)
+        right = evaluate_expr_range(expr.right)
+        if expr.type == ADD:
+            return IterVar("", left.start + right.start, left.end + right.end)
+        if expr.type == SUB:
+            return IterVar("", left.start - right.start, left.end - right.end)
+        elif expr.type == MUL:
+            return IterVar("", left.start * right.start, (left.end - 1) * (right.end - 1))
+        else:
+            raise ValueError("Unsupported type.")
+    else:
+        return IterVar("", expr, expr + 1)
+
 
 def lower(tensor):
-    # infer_bound(tensor)
+    intervals = [evaluate_expr_range(index, 0) for index in tensor.index]
+    infer_bound(tensor, intervals)
     return tensor.CUDA_codegen()
 
 if __name__ == "__main__":
