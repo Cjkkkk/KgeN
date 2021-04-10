17f87f6dcf33a6bbe1a21619310757c7e0d4f316
cjkkkk <shanbinke@gmail.com>
2021-04-10 18:06:12 +0800

    add expr simplifier evaluation

--- KgeN/expr_simplifier.py
+++ KgeN/expr_simplifier.py
@@ -1,52 +1,87 @@
-import sympy
-from .core import *
+from .tir import *
 
-def convert_to_sympy_expr(expr, reverse_map):
-    if isinstance(expr, IterVar):
-        sym = sympy.Symbol(expr.name)
-        reverse_map[sym] = expr
-        return sym
-    elif isinstance(expr, ConstExpr):
-        return expr.val
-    elif isinstance(expr, BinaryExpr):
-        if expr.type == BinaryExpr.ADD:
-            return convert_to_sympy_expr(expr.left, reverse_map) + convert_to_sympy_expr(expr.right, reverse_map)
-        elif expr.type == BinaryExpr.SUB:
-            return convert_to_sympy_expr(expr.left, reverse_map) - convert_to_sympy_expr(expr.right, reverse_map)
-        elif expr.type == BinaryExpr.MUL:
-            return convert_to_sympy_expr(expr.left, reverse_map) * convert_to_sympy_expr(expr.right, reverse_map)
-        elif expr.type == BinaryExpr.FLOOR_DIV:
-            return convert_to_sympy_expr(expr.left, reverse_map) // convert_to_sympy_expr(expr.right, reverse_map)
+class Pattern(Expr):
+    def __init__(self, cls_):
+        self.is_matched = False
+        self.expr = None
+        self.cls = cls_
+    
+    def match(self, other):
+        if self.is_matched:
+            return self.expr.same_as(other)
+        else:      
+            if isinstance(other, self.cls):      
+                self.is_matched = True
+                self.expr = other
+                return True
+            else:
+                return False
+
+    def reset(self):
+        self.is_matched = False
+        self.expr = None
+
+def real_match(expr, pattern):
+    if isinstance(pattern, BinaryExpr):
+        if isinstance(expr, BinaryExpr):
+            if pattern.type != expr.type:
+                return False
+            ans_left = real_match(expr.left, pattern.left)
+            ans_right = real_match(expr.right, pattern.right)
+            return ans_left and ans_right
         else:
-            pass
+            return False
     else:
-        pass
+        return pattern.match(expr)
 
-def convert_to_KgeN_expr(expr, reverse_map):
-    if isinstance(expr, sympy.core.symbol.Symbol):
-        return reverse_map[expr]
-    elif isinstance(expr, sympy.core.numbers.Integer):
-        return ConstExpr(expr.p)
-    elif isinstance(expr, sympy.core.mul.Mul):
-        left = convert_to_KgeN_expr(expr.args[0], reverse_map)
-        right = convert_to_KgeN_expr(expr.args[1], reverse_map)
-        return left * right
-    elif isinstance(expr, sympy.core.add.Add):
-        left = convert_to_KgeN_expr(expr.args[0], reverse_map)
-        right = convert_to_KgeN_expr(expr.args[1], reverse_map)
-        return left + right
+mmap = {
+    Expr.ADD: Expr.__add__
+}
+
+def eval_result(result):
+    if isinstance(result, BinaryExpr):
+        return mmap[result.type](
+            eval_result(result.left), 
+            eval_result(result.right))
     else:
-        pass
+        return result.expr
 
-def simplify(expr):
-    reverse_map = {}
-    sympy_expr = convert_to_sympy_expr(expr, reverse_map)
-    sympy_expr = sympy.simplify(sympy_expr)
-    expr = convert_to_KgeN_expr(sympy_expr, reverse_map)
+def rewrite_recursive(expr, pattern, result):
+    while True:
+        new_expr = rewrite(expr, pattern, result)
+        if new_expr.same_as(expr):
+            break
+        expr = new_expr
+
+    if isinstance(expr, BinaryExpr):
+        expr.left = rewrite_recursive(expr.left, pattern, result)
+        expr.right = rewrite_recursive(expr.right, pattern, result)
     return expr
 
+def reset_pattern(pattern):
+    if isinstance(pattern, BinaryExpr):
+        reset_pattern(pattern.left)
+        reset_pattern(pattern.right)
+    else:
+        pattern.reset()
+
+def rewrite(expr, pattern, result):
+    ans = real_match(expr, pattern)
+    if ans:
+        expr = eval_result(result)
+    reset_pattern(pattern)
+    return expr
 
 if __name__ == "__main__":
-    expr = ConstExpr(1) + IterVar("x", 0, 0) - ConstExpr(1) 
-    print(simplify(expr))
+    C1 = Pattern(ConstExpr)
+    C2 = Pattern(ConstExpr)
+    V1 = Pattern(Expr)
+    V2 = Pattern(Expr)
+    expr = IterVar("x", 0, 0) + ConstExpr(1) + ConstExpr(10) + ConstExpr(100)
+    expr = rewrite_recursive(expr, (V1 + C1) + C2, V1 + (C1 + C2))
+    print(expr)
 
+
+    expr = ((IterVar("x", 0, 0) + ConstExpr(1)) + ConstExpr(10)) - (IterVar("x", 0, 0) + ConstExpr(1))
+    expr = rewrite_recursive(expr, (V1 + V2) - V1, V2)
+    print(expr)
