016c9291a3ddba3b01ba5ef1bbfdb633a9eaa203
cjkkkk <shanbinke@gmail.com>
2021-12-26 18:38:42 -0800

    remove attach path from Stage

--- example/compute_inline.py
+++ example/compute_inline.py
@@ -5,8 +5,6 @@ C = te.compute((5, 16), lambda ci, cj : ci * cj, name='C')
 D = te.compute((5, 16), lambda di, dj : C[di, dj]*2, name='D')
 
 s = te.create_schedule(D.op)
-# s[C].compute_at(s[D], s[D].op.axis[1])
-print(s[C], s[D], id(s[D].op.axis[1]))
 s[C].compute_inline()
 func = KgeN.lower(s, [D])
 print(KgeN.build(func))
